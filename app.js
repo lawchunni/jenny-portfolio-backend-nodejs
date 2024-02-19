59dca58d50f6f29141d855e2928cae9ee4300d86
Pui Ching Chung <lawchunni@yahoo.com.hk>
2024-02-19 17:34:16 -0600

    Create basic database connection

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const { connectToDb, getDb } = require('./connect');
+
+// init app & middleware
+const app = express();
+
+// db connection
+let db;
+
+connectToDb((err) => {
+  if (!err) {
+    app.listen(4000, () => {
+      console.log('app listening on port 4000');
+    })
+    db = getDb();
+  }
+})
+
+// routes
+app.get('/portfolio', (req, res) => {
+  let records = [];
+
+  db.collection('portfolio')
+    .find() // return cursor
+    .sort({ _id: 1 })
+    .forEach(record => records.push(record))
+    .then(() => {
+      res.status(200).json(records)
+    })
+    .catch(() => {
+      res.status(500).json({error: 'Could not fetch the document'});
+    })
+
+})
+
